70b3f35c2e81d80d35805d60b38ad2a625e88a85
Allan San <allanterzisanches@gmail.com>
2024-07-29 16:31:23 -0300

    Creating the template, view, and url to display single contact.

--- contact/views/contact.py
+++ contact/views/contact.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
 from contact.models import Contact
+
 
 def index(request):
     contacts = Contact.objects \
@@ -10,8 +13,14 @@ def index(request):
         'contacts': contacts
     }
 
-    return render(
-        request,
-        'contact/index.html',
-        context
-    )
+    return render(request, 'contact/index.html', context)
+
+
+def contact(request, contact_id):
+        single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+        context = {
+            'contact': single_contact
+        }
+
+        return render(request, 'contact/contact.html', context)
