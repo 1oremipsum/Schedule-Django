7647030e59fb8c5fea25edcbfda5a82cb41bf7bf
Allan San <allanterzisanches@gmail.com>
2024-08-27 17:58:21 -0300

    Validating Form Fields with Clean, clean_field, and ValidationError

--- contact/forms.py
+++ contact/forms.py
@@ -8,7 +8,21 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ('first_name', 'last_name', 'phone', 'email',)
     
+    # More than one field required for validation
     def clean(self) -> dict[str, Any]:
-        # cleaned_data = self.cleaned_data
-        self.add_error('first_name', ValidationError('Error message', code='invalid'))
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
+
+        if first_name == last_name:
+            self.add_error('last_name', ValidationError('First name and last name cannot be the same', code='invalid'))
+
         return super().clean()
+    
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if len(first_name) < 2:
+            self.add_error('first_name', ValidationError('First name must be at least 2 characters long', code='invalid'))
+
+        return first_name
