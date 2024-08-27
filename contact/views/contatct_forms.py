0774947365ec5028c5d1aea6f8e8dfe4c0b05075
Allan San <allanterzisanches@gmail.com>
2024-08-27 14:57:13 -0300

    form for register contact (Part 2)

--- contact/views/contatct_forms.py
+++ contact/views/contatct_forms.py
@@ -1,4 +1,6 @@
-from django.shortcuts import redirect, render, get_object_or_404
+from typing import Any
+from django.shortcuts import redirect, render
+from django.core.exceptions import ValidationError
 from contact.models import Contact
 
 from django import forms
@@ -6,9 +8,12 @@ from django import forms
 class ContactForm(forms.ModelForm):
     class Meta():
         model = Contact
-        fields = (
-            'first_name', 'last_name', 'phone', 'email',
-        )
+        fields = ('first_name', 'last_name', 'phone', 'email',)
+    
+    def clean(self) -> dict[str, Any]:
+        cleaned_data = self.cleaned_data
+        self.add_error('first_name', ValidationError('Error message', code='invalid'))
+        return super().clean()
 
 
 def create(request):
@@ -18,3 +23,9 @@ def create(request):
         }
 
         return render(request, 'contact/create.html', context)
+    
+    context = {
+            'form': ContactForm()
+        }
+    
+    return render(request, 'contact/create.html', context)
