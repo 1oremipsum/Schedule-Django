1b46a65817b929e7a7257fb74d749ec508a224c5
Allan San <allanterzisanches@gmail.com>
2024-11-24 18:29:04 -0300

    Login and logout system + AuthenticationForm and django.contrib.auth

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
-from django.contrib import messages
+from django.contrib import auth, messages
+
 from contact.forms import RegisterForm
 
 def register(request):
@@ -11,8 +13,7 @@ def register(request):
             form.save()
             messages.success(request, 'New user successfully registered!')
             return redirect('contact:index')
-        else:
-            messages.error(request, 'Error registering new user')
+        messages.error(request, 'Error registering new user')
         
     return render(
         request,
@@ -21,3 +22,28 @@ def register(request):
             'form': form
         }
     )
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+           user = form.get_user()
+           auth.login(request, user)
+           messages.success(request, f"Login successfully, welcome {user.first_name}!")
+           return redirect('contact:index')
+        messages.error(request, "Invalid username or password")
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
